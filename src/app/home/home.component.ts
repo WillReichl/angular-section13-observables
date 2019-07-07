dd495d2f5c5b8fe4db896b46e2a8db1d3402f98b
Will Reichl <willreichl@gmail.com>
2019-07-06 22:12:26 -0400

    Interval Observable and Manual Unsubscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private firstObsSubscription: Subscription;
+  count: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe(
+      count => {
+        console.log(count);
+        this.count = count;
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    // We must clean up our own observable or it will continue to run even if we navigate away.
+    // It can even start multiple threads that run in parallel. Memory leaks and resource issues.
+    this.firstObsSubscription.unsubscribe();
+  }
 }
